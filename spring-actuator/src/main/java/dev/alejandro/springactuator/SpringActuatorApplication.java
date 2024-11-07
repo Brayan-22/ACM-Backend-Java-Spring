e6a9f889ed27cf6ecb0ddf3d5edde5d9de8095e9
alejandro <alejandroriveros100@gmail.com>
2024-11-06 19:46:56 -0500

    [fix] security config

--- spring-actuator/src/main/java/dev/alejandro/springactuator/SpringActuatorApplication.java
+++ spring-actuator/src/main/java/dev/alejandro/springactuator/SpringActuatorApplication.java
@@ -1,11 +1,16 @@
 package dev.alejandro.springactuator;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 
 @SpringBootApplication
-public class SpringActuatorApplication {
 
+public class SpringActuatorApplication {
+	private static final Logger log = LoggerFactory.getLogger(SpringActuatorApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(SpringActuatorApplication.class, args);
 	}
--- spring-actuator/src/main/java/dev/alejandro/springactuator/config/SecurityConfig.java
+++ spring-actuator/src/main/java/dev/alejandro/springactuator/config/SecurityConfig.java
@@ -2,9 +2,20 @@ package dev.alejandro.springactuator.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.ObjectPostProcessor;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,12 +25,45 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .authenticationProvider(authenticationProvider())
                 .csrf(c -> c.disable())
                 .authorizeHttpRequests(auth->{
                     auth.requestMatchers("/actuator/**").permitAll();
                     auth.anyRequest().authenticated();
                 })
+                .sessionManagement(session->{
+                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                    session.maximumSessions(1);
+                })
                 .httpBasic(Customizer.withDefaults())
                 .build();
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService());
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("alejandro")
+                        .password(new BCryptPasswordEncoder().encode("1234"))
+                        .build()
+        );
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
